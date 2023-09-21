f00f2dba57a6144ff23ea242429b0356975f7129
Ryan Lee <ryan.lee@mavenwave.com>
2023-09-21 10:53:02 -0400

    remove vpc connector

--- modules/networking/vpc_connector.tf
+++ modules/networking/vpc_connector.tf
@@ -9,9 +9,9 @@
   machine_type = var.vpc_machine_type
 }
 */
-resource "google_compute_subnetwork" "custom_test" {
-  name          = var.vpc_con_subnet
-  ip_cidr_range = var.vpc_connector
-  region        = var.region[1]
-  network       = var.network
-}
+# resource "google_compute_subnetwork" "custom_test" {
+#   name          = var.vpc_con_subnet
+#   ip_cidr_range = var.vpc_connector
+#   region        = var.region[1]
+#   network       = var.network
+# }
